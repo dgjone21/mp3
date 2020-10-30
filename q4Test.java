22e4a73749c000922c97e5e9d515aa40f6c267d9
Dalen Jones <39420799+dgjone21@users.noreply.github.com>
2020-10-30 17:03:22 -0400

    Add files via upload
    Q4

---
+++ q4Test.java
@@ -0,0 +1,9 @@
+package jenkins.model;
+
+public class q4Test {
+    IdStrategy test1 = new IdStrategy;
+    test1.idFromFilename("testfile");
+
+    IdStrategy test2 = new IdStrategy;
+    test1.idFromFilename(null);
+}
